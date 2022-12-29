deed4858d9342752524b9fc86c7480ac67ac2587
AZ Software <66299945+AZProductions@users.noreply.github.com>
2022-12-29 15:41:06 +0100

    Windows Implementation without a timer

--- src/windows.rs
+++ src/windows.rs
@@ -10,13 +10,12 @@ pub struct Timer {}
 
 impl Timer {
     pub fn new(interval: f64, func: Box<dyn Fn()>) -> Box<Self> {
-        todo!()
+        Box::new(Self{})
     }
 }
 
 impl Drop for Timer {
     fn drop(&mut self) {
-        todo!()
     }
 }
 
@@ -25,16 +24,16 @@ unsafe impl Send for Timer {}
 
 type Callback = Box<dyn Fn(&str)>;
 
-impl NativeWebView {
-    pub fn new(handle: RawWindowHandle, source: Arc<HTMLSource>, size: (u32, u32), callback: Callback) -> Self {
-        todo!()
-    }
+// impl NativeWebView {
+//     pub fn new(handle: RawWindowHandle, source: Arc<HTMLSource>, size: (u32, u32), callback: Callback) -> Self {
+//         todo!()
+//     }
     
-    pub fn set_size(&mut self, size: (u32, u32)) {
-        todo!()
-    }
+//     pub fn set_size(&mut self, size: (u32, u32)) {
+//         todo!()
+//     }
     
-    pub fn evaluate_javascript(&self, js: &str) {
-        todo!()
-    }
-}
+//     pub fn evaluate_javascript(&self, js: &str) {
+//         todo!()
+//     }
+// }
