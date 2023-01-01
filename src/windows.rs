42b096d4b1a0244e8390514054b1b26d106f5e36
AZ Software <66299945+AZProductions@users.noreply.github.com>
2023-01-01 18:07:05 +0100

    chore: removed whitespace

--- src/windows.rs
+++ src/windows.rs
@@ -34,17 +34,6 @@ unsafe impl Send for Timer {}
 
 extern "system" fn callback(hwnd: HWND, uMsg: UINT, idEvent: UINT_PTR, dwTime: DWORD){
     pub static mut _clb: *mut UINT = 0 as *const UINT as *mut UINT;
-    // let mut file = std::fs::OpenOptions::new()
-    //     .write(true)
-    //     .append(true)
-    //     .open("D:/VST/file.txt")
-    //     .unwrap();
-    //     if let Err(e) = writeln!(file, "{:?}", timer) {
-    //         eprintln!("Couldn't write to file: {}", e);
-    //     }
-
-    
-
     unsafe{
         let timer = _clb as *mut Timer;
         ((*timer).func)();
