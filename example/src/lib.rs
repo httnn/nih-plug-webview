1abc669cc18baf39d69f5c142a7046a693bc38bd
max <max.huttunen@gmail.com>
2023-01-03 21:55:31 +0200

    update packages

--- example/src/lib.rs
+++ example/src/lib.rs
@@ -130,7 +130,7 @@ impl Plugin for Gain {
             let _ = ctx.send_json(json!({
                 "type": "param_change",
                 "param": "gain",
-                "value": params.gain.normalized_value()
+                "value": params.gain.value()
             }));
         })
         .build();
