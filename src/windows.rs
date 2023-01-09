3a2cd5289fe886ca849c7bf5e93c00392aa23337
AZ Software <66299945+AZProductions@users.noreply.github.com>
2023-01-09 19:53:14 +0100

    fixed the code

--- src/windows.rs
+++ src/windows.rs
@@ -11,19 +11,15 @@ use std::io::prelude::*;
 
 pub struct Timer {
     handle: *mut c_void,
-    id_event: Option<*mut c_void>,
     func: Box<dyn FnMut()>
 }
 
 impl Timer {
     pub fn new(handle: *mut c_void, interval: f64, func: Box<dyn FnMut()>) -> Box<Self> {
-        let mut timer =  Box::new(Self { handle, func, id_event: None });
-        timer.id_event = Some(&mut timer as *mut _ as *mut c_void);
-
-        let ptr_num_transmute = unsafe { std::mem::transmute::<*mut c_void, UINT_PTR>(timer.id_event.unwrap()) };
+        let mut timer =  Box::new(Self { handle, func});
 
         unsafe {
-            let test = SetTimer(handle as HWND, ptr_num_transmute, interval as u32, Some(callback));
+            let test = SetTimer(handle as HWND, &mut *timer as *mut _ as UINT_PTR, interval as u32, Some(callback));
             // std::fs::write("D:/VST/debug/new.txt", test.to_string()).expect("Unable to write file");
         }
 
@@ -35,9 +31,9 @@ impl Timer {
 impl Drop for Timer {
     fn drop(&mut self) {
         unsafe { 
-            let ptr_num_transmute = unsafe { std::mem::transmute::<*mut c_void, usize>(self.id_event.unwrap()) };
+            // let ptr_num_transmute = unsafe { std::mem::transmute::<*mut c_void, usize>(self.id_event.unwrap()) };
 
-            KillTimer(self.handle as HWND, ptr_num_transmute); 
+            // KillTimer(self.handle as HWND, ptr_num_transmute); 
         }
     }
 }
@@ -46,10 +42,8 @@ unsafe impl Sync for Timer {}
 unsafe impl Send for Timer {}
 
 extern "system" fn callback(hwnd: HWND, u_msg: UINT, id_event: UINT_PTR, dw_time: DWORD){
-    let ptr_num_transmute = unsafe { std::mem::transmute::<UINT_PTR, *mut c_void>(id_event) };
-
     unsafe{
-        let timer = ptr_num_transmute as *mut Timer;
+        let timer = id_event as *mut Timer;
         ((*timer).func)();
         // std::fs::write("D:/VST/debug/callback.txt", id_event.to_string()).expect("Unable to write file");
     }
